eec813217ce22badf33a36a8f24a756564c3c4aa
Gt9ight <marianosanchez0307@gmail.com>
2024-01-25 01:17:54 -0500

    added call function

--- src/components/Roadside.js
+++ src/components/Roadside.js
@@ -2,10 +2,16 @@ import React from 'react';
 import './roadside.css';
 
 const Roadside = () => {
+    const phoneNumber = '+1234567890';
+  
+    const handleCallClick = () => {
+      window.location.href = `tel:${phoneNumber}`;
+    };
+  
   return (
     <div className='roadside-banner'>
       <h1>Get 24/7 Roadside assistance!</h1>
-      <button className='roadside-button'>Roadside Assistance</button>
+      <button onClick={handleCallClick} className='roadside-button'>Roadside Assistance</button>
     </div>
   );
 };
